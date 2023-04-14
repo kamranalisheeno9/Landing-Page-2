4dabd65ac339b4a8ca669e4e95809587223fd47c
kamranalisheeno9 <kamranalisheeno9@gmail.com>
2023-04-14 07:36:26 +0500

    Custom Pages updated 2

--- src/Pages/PageOne.jsx
+++ src/Pages/PageOne.jsx
@@ -30,32 +30,41 @@ const PageOne = () => {
         Creating visually stunning and memorable UI/UX interfaces that leave a
         lasting impression, while also solving real-world problems
       </div>
-      <div className="mood_tracker">
+      <div className="traker_page">
         <Link className="page_2" to="page2">
-          <Row className="mood_row justify-content-between">
-            <Col md={6} className="mood_mob_col">
-              <img className="mood_mob_img" src={MoodMobileImg} alt="Mobile" />
-            </Col>
-            <Col md={6}>
-              <div className="mood_text">Mood Tracker</div>
+          <div className="mood_text">Mood Tracker</div>
               <div className="mood_text_para">MOBILE APP UI/UX</div>
-              <img className="layer_img_1" src={MoodLayerImg1} alt="Layer 1" />
-              <img className="layer_img_2" src={MoodLayerImg2} alt="Layer 2" />
+        
+          <Row className="justify-content-around">
+            <Col className="mood_img_col" md={6} lg={5}>
+            <img className="mood_mob_img" src={MoodMobileImg} alt="Mobile" />
             </Col>
+        <Col md={6} lg={6}>
+
+          <div className=" m-5">
+
+            <img className="layer_img_1 " src={MoodLayerImg1} alt="Layer 1" />
+              <img className="layer_img_2" src={MoodLayerImg2} alt="Layer 2" />
+          </div>
+
+        </Col>
           </Row>
+       
         </Link>
       </div>
 
-      <Row className="justify-content-between">
-        <Col md={7} className="spotify">
-          <div className="spotify_text">Spotify</div>
-          <div className="spotify_text_para">Redesign UI/UX</div>
-          <img className="spotify_img" src={SpotifyImg} alt="Spotify" />
+      <Row className="justify-content-around">
+        <Col lg={6} xl={6} className="spotify_container">
+           <div className="spotify_text">Spotify</div>
+        <div className="spotify_text_para">Redesign UI/UX</div>
+      <img className="spotify_img" src={SpotifyImg} alt="Spotify" /> 
+
         </Col>
-        <Col className="finance">
-          <div className="finance_text">Finance</div>
+        <Col lg={5} xl={4} className="finance_container">
+        <div className="finance_text">Finance</div>
           <div className="finance_text_para">BUDGET TRACKER UI/UX</div>
           <img className="finance_img" src={FinanceImg} alt="Spotify" />
+     
         </Col>
       </Row>
       <div className="copyright">
